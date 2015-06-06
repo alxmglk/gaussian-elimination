444aae8452cf36bea0d6cf1f7f35201de33f6fb3
alxmglk <alxmfr0st@gmail.com>
2015-06-06 16:52:01 +0300

    Allocate contiguous array for matrix inside LinearEquationSystem to support collective operations based on the usage of MPIRowType

--- Sources/LinearEquationSystem.cpp
+++ Sources/LinearEquationSystem.cpp
@@ -11,20 +11,18 @@ LinearEquationSystem::LinearEquationSystem(int n, int rowsCount)
 	RowsCount = rowsCount;
 	ColumnsCount = n + 1;
 	FreeTermIndex = n;
+
+	NUMBER* data = (NUMBER*)_aligned_malloc(RowsCount * ColumnsCount * sizeof(NUMBER), 16);
 	AugmentedMatrix = (NUMBER**)malloc(RowsCount * sizeof(NUMBER*));
 
 	for (int row = 0; row < RowsCount; ++row)
 	{
-		AugmentedMatrix[row] = (NUMBER*)_aligned_malloc(ColumnsCount * sizeof(NUMBER), 16);
+		AugmentedMatrix[row] = &(data[row * ColumnsCount]);
 	}
 }
 
 LinearEquationSystem::~LinearEquationSystem()
 {
-	for (int row = 0; row < RowsCount; ++row)
-	{
-		_aligned_free(AugmentedMatrix[row]);
-	}
-
+	_aligned_free(AugmentedMatrix[0]);
 	free(AugmentedMatrix);
 }
